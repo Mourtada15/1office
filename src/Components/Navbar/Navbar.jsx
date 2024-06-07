2f0b91a4185fc0b4791dd53ddfa6a88cdeb41196
Hassan Mourtada <h.mourtada@1world.xyz>
2024-06-07 14:43:08 +0300

    Added the navbar, and a section

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import "./Navbar.css";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const NavbarMenu = () => {
+
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#"><img src="/1O_Logo_Draft2.png" alt="logo" width={'100px'}/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">HOME</Nav.Link>
+            <Nav.Link href="#action2">ABOUT US</Nav.Link>
+            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown> */}
+            <Nav.Link href="#" >PACKAGES</Nav.Link>
+            <Nav.Link href="#" >GALLERY</Nav.Link>
+            <Nav.Link href="#" >CONTACT US</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarMenu;
---
+++ src/Components/Sections/Aboutus/Aboutus.jsx
@@ -0,0 +1,49 @@
+import "./Aboutus.css";
+
+const Aboutus = () => {
+  return (
+    <div className="aboutus-wrapper">
+      <h1>One Office is a coworking-space in the heart of Beirut</h1>
+      <p>1O is youir easy access to begin your business. 1O grants you a space suitable for all your business needs. At 1O, you have peace of mind regarding major running (Overhead) costs since we provide all the basics free of charge.</p>
+      <div className="aboutus-container">
+        <div className="inner-div">
+          <div>
+            <img src="" alt="" />
+            <h4>Consultancy</h4>
+            <p className="col-md-6">We care about our customer's product and offer free updates and the best support around.</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h4>Fully Customizable</h4>
+            <p className="col-md-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, voluptatem voluptate cum autem optio nam dignissimos repellendus voluptatum inventore tempora </p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h4>Code Consistency</h4>
+            <p className="col-md-6">We care about our customer's product and offer free updates and the best support around.</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <img src="" alt="" />
+            <h4>Branding</h4>
+            <p className="col-md-6">We care about our customer's product and offer free updates and the best support around.</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h4>Video Production</h4>
+            <p className="col-md-6">We care about our customer's product and offer free updates and the best support around.</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h4>Sales Support</h4>
+            <p className="col-md-6">We care about our customer's product and offer free updates and the best support around.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Aboutus;
