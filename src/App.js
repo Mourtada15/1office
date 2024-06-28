4b5d0d3d527f132ed04f010bc683166d8dbbf182
Hassan Mourtada <h.mourtada@1world.xyz>
2024-06-28 14:50:53 +0300

    some updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
 import Aboutus from './Components/Sections/Aboutus/Aboutus';
+import Packages from './Components/Sections/Packages/Packages';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className='app-wrapper-body'>
         <Aboutus />
       </div>
+      <Packages />
       <Footer />
     </div>
   );
